da1f3be596b837f6bdd80c5a5195ae24998373bb
Pol Cadavid <polkadavid@gmail.com>
2023-06-26 22:38:20 +0200

    Change started

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,50 @@
 <template>
   <div>
-    <header class="shadow-sm bg-secondary-50">
-      <nav class="container mx-auto p-4 flex items-center justify-between">
+    <div>
+      <header class="shadow-sm mx-auto px-4 flex items-center justify-between">
         <nuxt-link to="/" class="">
-          <nuxt-icon v-if="themeStore.isDarkTheme" name="logo-text-dark" filled />
+          <nuxt-icon
+            v-if="themeStore.isDarkTheme"
+            name="logo-text-dark"
+            filled
+          />
           <nuxt-icon v-else name="logo-text-light" filled />
         </nuxt-link>
-        <div class="flex gap-4 items-center">
+        <nav class="flex gap-4 items-center">
           <ul class="flex gap-4">
             <li><nuxt-link to="/">Home</nuxt-link></li>
             <li><nuxt-link to="/about">About</nuxt-link></li>
             <li><nuxt-link to="/contact">Contact</nuxt-link></li>
             <li><nuxt-link to="/design" class="btn">Design</nuxt-link></li>
           </ul>
-          <button class="flex"
-            @click="themeStore.toggleTheme()"
-          >
+          <button class="flex" @click="themeStore.toggleTheme()">
             <i
               v-if="themeStore.isDarkTheme"
               class="material-icons-outlined text-surface-900"
               >light_mode</i
             >
-            <i v-else class="material-icons-outlined text-surface-900">dark_mode</i>
+            <i v-else class="material-icons-outlined text-surface-900"
+              >dark_mode</i
+            >
           </button>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </div>
 
     <!-- Here starts the page content -->
-    <div class="container mx-auto p-4">
+    <main
+      class="prose prose-stone dark:prose-invert md:prose-lg lg:prose-xl container mx-auto p-4"
+    >
       <slot />
-    </div>
+    </main>
     <!-- Here ends the page content -->
 
-    <footer class="bg-secondary-50">
-      <nav
-        class="container mx-auto p-4 flex items-center justify-between border-t-2"
-      >
-        <nuxt-link to="/" class="font-bold">
-          <nuxt-icon v-if="themeStore.isDarkTheme" name="logo-text-dark" filled />
-          <nuxt-icon v-else name="logo-text-light" filled />
-        </nuxt-link>
+    <footer class="mx-auto px-4 flex items-center justify-between border-t-2">
+      <nuxt-link to="/" class="font-bold">
+        <nuxt-icon v-if="themeStore.isDarkTheme" name="logo-text-dark" filled />
+        <nuxt-icon v-else name="logo-text-light" filled />
+      </nuxt-link>
+      <nav>
         <ul class="flex gap-4">
           <li><nuxt-link to="/">Home</nuxt-link></li>
           <li><nuxt-link to="/about">About</nuxt-link></li>
@@ -53,7 +57,7 @@
 </template>
 
 <script setup>
-import { useThemeStore } from '../stores/themeStore';
+import { useThemeStore } from "../stores/themeStore";
 
 const themeStore = useThemeStore();
 </script>
--- layouts/design.vue
+++ layouts/design.vue
@@ -1,41 +1,53 @@
 <template>
   <div>
-    <header class="shadow-sm bg-secondary-50">
-      <nav class="container mx-auto p-4 flex items-center justify-between">
+    <header class="shadow-sm">
+      <nav class="mx-auto px-4 flex items-center justify-between">
         <nuxt-link to="/" class="">
-          <nuxt-icon v-if="themeStore.isDarkTheme" name="logo-text-dark" filled />
+          <nuxt-icon
+            v-if="themeStore.isDarkTheme"
+            name="logo-text-dark"
+            filled
+          />
           <nuxt-icon v-else name="logo-text-light" filled />
         </nuxt-link>
         <div class="flex gap-4 items-center">
-        <ul class="flex gap-4">
-          <li><nuxt-link to="/design/palette">Palette</nuxt-link></li>
-          <li><nuxt-link to="/design/typography">Trypography</nuxt-link></li>
-          <li><nuxt-link to="/design" class="btn">Design</nuxt-link></li>
-        </ul>
-        <button class="flex"
-            @click="themeStore.toggleTheme()"
-          >
+          <ul class="flex gap-4">
+            <li><nuxt-link to="/design/palette">Palette</nuxt-link></li>
+            <li><nuxt-link to="/design/typography">Trypography</nuxt-link></li>
+            <li><nuxt-link to="/design" class="btn">Design</nuxt-link></li>
+          </ul>
+          <button class="flex" @click="themeStore.toggleTheme()">
             <i
               v-if="themeStore.isDarkTheme"
               class="material-icons-outlined text-surface-900"
               >light_mode</i
             >
-            <i v-else class="material-icons-outlined text-surface-900">dark_mode</i>
+            <i v-else class="material-icons-outlined text-surface-900"
+              >dark_mode</i
+            >
           </button>
-      </div>
+        </div>
       </nav>
     </header>
 
     <!-- Here starts the page content -->
-    <div class="container mx-auto p-4">
+    <main
+      class="prose prose-stone dark:prose-invert md:prose-lg lg:prose-xl container mx-auto p-4"
+    >
       <slot />
-    </div>
+    </main>
     <!-- Here ends the page content -->
 
-    <footer class="bg-secondary-50">
-      <nav class="container mx-auto p-4 flex items-center justify-between border-t-2">
+    <footer class="">
+      <nav
+        class="container mx-auto px-4 flex items-center justify-between border-t-2"
+      >
         <nuxt-link to="/" class="">
-          <nuxt-icon v-if="themeStore.isDarkTheme" name="logo-text-dark" filled />
+          <nuxt-icon
+            v-if="themeStore.isDarkTheme"
+            name="logo-text-dark"
+            filled
+          />
           <nuxt-icon v-else name="logo-text-light" filled />
         </nuxt-link>
         <ul class="flex gap-4">
@@ -49,7 +61,7 @@
 </template>
 
 <script setup>
-import { useThemeStore } from '../stores/themeStore';
+import { useThemeStore } from "../stores/themeStore";
 
 const themeStore = useThemeStore();
 </script>
