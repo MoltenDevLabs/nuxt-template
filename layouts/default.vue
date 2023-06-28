c679cbe603498591ce8a5b2b4051fe161140129b
Pol Cadavid <polkadavid@gmail.com>
2023-06-28 12:28:52 +0200

    Typography styles and hamburger menu for mobile done

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,6 @@
 <template>
   <div>
-    <header
-      v-if="navMenu"
-      class="absolute top-[72px] min-w-full z-10 overflow-hidden transition-all ease-out duration-500"
-      :class="navMenu ? 'max-h-[300px]' : 'max-h-0'"
-    >
-      <ul class="flex flex-col items-center p-0 duration-300 ease-out">
-        <li><nuxt-link to="/" @click="toggleMenu">Home</nuxt-link></li>
-        <li>
-          <nuxt-link to="/about" @click="toggleMenu">About</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/contact" @click="toggleMenu">Contact</nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/design" @click="toggleMenu" class="btn"
-            >Design</nuxt-link
-          >
-        </li>
-      </ul>
-    </header>
-
+    <!-- HEADER START -->
     <div v-if="$device.isMobile">
       <header class="mx-auto px-8 flex items-center justify-between">
         <nuxt-link to="/" @click="navMenu = false">
@@ -32,6 +12,35 @@
               navMenu ? "close" : "menu"
             }}</i>
           </button>
+        </ul>
+      </header>
+      <header
+        class="absolute top-[72px] min-w-full h-0 z-10 overflow-hidden transition-all ease-out duration-500"
+        :class="navMenu ? 'h-56 opacity-90' : 'h-0'"
+      >
+        <ul class="flex flex-col items-center p-0">
+          <button class="absolute top-2 right-9 flex" @click="themeStore.toggleTheme()">
+            <i
+              v-if="themeStore.isDarkTheme"
+              class="material-icons-outlined text-surface-900"
+              >light_mode</i
+            >
+            <i v-else class="material-icons-outlined text-surface-900"
+              >dark_mode</i
+            >
+          </button>
+          <li><nuxt-link to="/" @click="toggleMenu">Home</nuxt-link></li>
+          <li>
+            <nuxt-link to="/about" @click="toggleMenu">About</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/contact" @click="toggleMenu">Contact</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/design" @click="toggleMenu" class="btn"
+              >Design</nuxt-link
+            >
+          </li>
         </ul>
       </header>
     </div>
@@ -66,35 +75,63 @@
         </nav>
       </header>
     </div>
+    <!-- HEADER END -->
 
-    <!-- Here starts the page content -->
+    <!-- CONTENT START -->
     <main
       class="prose prose-stone dark:prose-invert md:prose-lg lg:prose-xl container mx-auto p-4"
+      :class="navMenu ? 'opacity-50 blur-sm' : ''"
     >
       <slot />
     </main>
-    <!-- Here ends the page content -->
+    <!-- CONTENT END -->
 
+    <!-- FOOTER START -->
     <div v-if="$device.isMobile">
-      <footer
-        class="mx-auto px-4 flex flex-col items-center justify-between border-t-2"
-      >
-        <nuxt-link to="/" class="font-bold">
-          <nuxt-icon
-            v-if="themeStore.isDarkTheme"
-            name="logo-text-dark"
-            filled
-          />
-          <nuxt-icon v-else name="logo-text-light" filled />
-        </nuxt-link>
-        <nav>
-          <ul class="flex flex-col gap-4">
-            <li><nuxt-link to="/">Home</nuxt-link></li>
-            <li><nuxt-link to="/about">About</nuxt-link></li>
-            <li><nuxt-link to="/contact">Contact</nuxt-link></li>
-            <li><nuxt-link to="/design" class="btn">Design</nuxt-link></li>
-          </ul>
-        </nav>
+      <footer class="mx-auto p-6 grid grid-cols-2 gap-4 justify-between border-t-2">
+        <div class="col-span-2">
+          <nuxt-link to="/" @click="scrollToTop">
+            <nuxt-icon
+              v-if="themeStore.isDarkTheme"
+              name="logo-text-dark"
+              filled
+            />
+            <nuxt-icon v-else name="logo-text-light" filled />
+          </nuxt-link>
+          <p class="text-xs mb-0">
+            Made with
+            <span class="material-icons-outlined align-text-bottom">local_fire_department</span>
+            in Reus
+          </p>
+        </div>
+        <div class="col-span-1">
+          <nav>
+            <ul>
+              <h6 class="font-bold text-xs mb-2">Check my work</h6>
+              <li class="text-xs"><nuxt-link to="/contact">Contact</nuxt-link></li>
+              <li class="text-xs"><nuxt-link to="https://github.com/MoltenDevLabs">Github</nuxt-link></li>
+              <li class="text-xs"><nuxt-link to="https://www.linkedin.com/in/pol-cadavid-rojas-09572366/">LinkedIn</nuxt-link></li>
+            </ul>
+            <ul>
+              <h6 class="font-bold text-xs mb-2">More links</h6>
+              <li class="text-xs"><nuxt-link to="/contact">Contact</nuxt-link></li>
+              <li class="text-xs"><nuxt-link to="https://github.com/MoltenDevLabs">Github</nuxt-link></li>
+              <li class="text-xs"><nuxt-link to="https://www.linkedin.com/in/pol-cadavid-rojas-09572366/">LinkedIn</nuxt-link></li>
+            </ul>
+          </nav>
+        </div>
+        <div class="col-span-1">
+          <nav>
+            <ul>
+              <h6 class="font-bold text-xs mb-2">Other Links</h6>
+              <li class="text-xs"><nuxt-link to="/contact">Contact</nuxt-link></li>
+            </ul>
+            <ul>
+              <h6 class="font-bold text-xs mb-2">More links</h6>
+              <li class="text-xs"><nuxt-link to="/contact">Contact</nuxt-link></li>
+            </ul>
+          </nav>
+        </div>
       </footer>
     </div>
 
@@ -118,6 +155,7 @@
         </nav>
       </footer>
     </div>
+    <!-- FOOTER END -->
   </div>
 </template>
 
@@ -128,6 +166,13 @@ const themeStore = useThemeStore();
 const navMenu = ref(false);
 const toggleMenu = () => {
   navMenu.value = !navMenu.value;
+};
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 };
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="h-96">
+  <div>
     <h1>Home View</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas natus laboriosam consectetur cumque repudiandae esse voluptas facere! Tempora deleniti facere praesentium quis ad, officiis cum eligendi omnis ratione fugit neque.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore magni, tempora vitae, quia nesciunt error nulla quas nostrum eum exercitationem minus. Quas, atque officiis voluptatum accusamus animi mollitia vitae voluptatibus.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse voluptas explicabo adipisci vel, possimus labore fugiat totam dicta libero iste ipsum, obcaecati quaerat. Voluptatem explicabo blanditiis eum! Nisi, pariatur debitis.</p>
   </div>
 </template>
 
@@ -10,8 +13,7 @@ const user = useSupabaseUser();
 const router = useRouter();
 
 watchEffect(() => {
-  if(user.value)
-  router.push('/')
+  if (user.value) router.push("/");
 });
 
 /* definePageMeta({
